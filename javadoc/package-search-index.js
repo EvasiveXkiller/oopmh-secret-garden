e6d07853e77250da330fb38970031a16f0314ee5
EvasiveXkiller <evasivexkiller@gmail.com>
2021-07-26 11:55:49 +0800

    Compiled Java doc, updating README soon

---
+++ javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"mainrunnable"},{"l":"secretGarden"},{"l":"secretGarden.database"},{"l":"secretGarden.enums"},{"l":"secretGarden.interfaces"},{"l":"secretGarden.items"},{"l":"secretGarden.utils"}];updateSearchResults();
